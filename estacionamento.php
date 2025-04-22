34a51894d5542d9593e3bc3a37ebb139828390fa
Aluno.Etec <aula.etec.com>
2025-04-22 16:59:51 -0300

    estacionamento

---
+++ estacionamento.php
@@ -0,0 +1,48 @@
+<HTML>
+<HEAD>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <TITLE>Incluir</TITLE>
+</HEAD>
+<BODY>
+<?php
+$placa=			$_POST["placa"];
+$veiculo=		$_POST["veiculo"];
+$marca= 		   $_POST["marca"];
+$cor=			   $_POST["cor"];
+$ano=			   $_POST["ano"];
+$proprietario=	$_POST["proprietario"];
+$fone=			$_POST["fone"];
+$opcionais=		$_POST["opcionais"];
+$con=mysqli_connect("localhost","root","","detran") or die ("erro!"); //faz a conexão com o banco de dados detran
+
+$in = "insert into veiculos values ('$placa',
+									         '$veiculo',
+									         '$marca',
+									         '$cor',
+									         '$ano',
+									         '$proprietario', 
+									         '$fone', 
+									         '$opcionais')";
+$incluir=mysqli_query($con,$in);
+if ($incluir==1)
+{
+   echo "
+   Placa:		$placa<BR>
+   Veículo:		$veiculo<BR>
+   Marca:		$marca<BR>
+   Cor:			$cor<BR>
+   Ano:			$ano<BR>
+   Proprietário:$proprietario<BR>
+   Fone:		$fone<BR>
+   Opcionais: 	$opcionais<hr>";
+   
+   echo "Registro incluído com sucesso!<BR>";
+}
+else
+{
+   echo "Registro NÃO incluído!<BR>";
+}
+echo "<a href='incluir.html'>Voltar</a><BR>";
+?>
+</BODY>
+</HTMl>
